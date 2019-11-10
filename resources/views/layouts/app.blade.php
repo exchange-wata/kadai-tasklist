1ec8ff3169719f9733fde9772c4019422a7e9d4d
EC2 Default User <ec2-user@ip-172-31-45-110.ec2.internal>
2019-11-10 07:57:39 +0000

    add CRUD files

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -19,6 +19,7 @@
                 <div class="collapse navbar-collapse" id="nav-bar">
                     <ul class="navbar-nav mr-auto"></ul>
                     <ul class="navbar-nav">
+                        {!! link_to_route('tasks.create', '新規タスクの追加', [], ['class' => 'btn btn-light']) !!}
                     </ul>
                 </div>
             </nav>
--- resources/views/tasks/create.blade.php
+++ resources/views/tasks/create.blade.php
@@ -2,4 +2,22 @@
 
 @section('content')
 
+    <h1>タスクの新規作成ページ</h1>
+    
+    <div class="row">
+        <div class="col-6">
+            
+            {!! Form::model($task, ['route' => 'tasks.store']) !!}
+            
+                <div class="form-group">
+                    {!! Form::label('content', 'タスク内容:') !!}
+                    {!! Form::text('content', null, ['class' => 'form-control']) !!}
+                </div>
+                
+                {!! Form::submit('投稿', ['class' => 'btn btn-light']) !!}
+            
+            {!! Form::close() !!}
+        </div>
+    </div>
+
 @endsection
