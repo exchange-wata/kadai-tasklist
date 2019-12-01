9b7b33bc12b1e813501ff0a213bd8808fc2346ab
EC2 Default User <ec2-user@ip-172-31-45-110.ec2.internal>
2019-12-01 06:31:33 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -25,5 +25,10 @@ Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
 Route::post('login', 'Auth\LoginController@login')->name('login.post');
 Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
 
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
 // Route::get('/', 'TasksController@index');
 Route::resource('tasks', 'TasksController');
